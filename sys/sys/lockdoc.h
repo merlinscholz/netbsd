5e79013d09158507e65aaea09ffebbef8249ab5e
Merlin Scholz <merlin@scholz.ruhr>
2022-10-17 22:44:37 +0200

    Continue implementation

--- sys/sys/lockdoc.h
+++ sys/sys/lockdoc.h
@@ -24,8 +24,47 @@ static inline void log_memory(int alloc, const char *datatype, const void *ptr, 
 
 }
 
-static inline void log_lock(int lock_op, const kmutex_t* ptr, const char *file, int line, int irq_sync) {
+static inline void log_lock(int lock_op, const void* ptr, const char *file, int line, int irq_sync) {
+
     memset(&la_buffer,0,sizeof(la_buffer));
+
+    // TODO fail shows 0x016c instead of 0x6c
+    // action
+    la_buffer.action = LOCKDOC_LOCK_OP;
+
+    // operation
+    la_buffer.lock_op = lock_op;
+
+    // pointer
+    la_buffer.ptr = (unsigned long)ptr;
+
+    // size only relevant for memory access
+
+    // TODO type
+    //strncpy(la_buffer.type, ,LOG_CHAR_BUFFER_LEN);
+	la_buffer.type[LOG_CHAR_BUFFER_LEN - 1] = '\0';
+
+    // TODO lock_member
+    strncpy(la_buffer.lock_member,"dummy",LOG_CHAR_BUFFER_LEN);
+	la_buffer.lock_member[LOG_CHAR_BUFFER_LEN - 1] = '\0';
+
+
+    // file
+    strncpy(la_buffer.file,file,LOG_CHAR_BUFFER_LEN);
+	la_buffer.file[LOG_CHAR_BUFFER_LEN - 1] = '\0';
+
+
+    // line
+    la_buffer.line = line;
+
+    // TODO function
+	strncpy(la_buffer.function,"dummy",LOG_CHAR_BUFFER_LEN);
+	la_buffer.function[LOG_CHAR_BUFFER_LEN - 1] = '\0';
+
+    // TODO preempt_count
+    // TODO irq_sync
+    // TODO flags
+
     outb_(PING_CHAR,IO_PORT_LOG);
 }
 #else
