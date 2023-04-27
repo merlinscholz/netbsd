79be5eb781cb7c6d4e6cc39de333501b295c37b4
Merlin Scholz <merlin@scholz.ruhr>
2023-04-27 12:39:19 +0200

    Fix GENERIC build and add proper bootloader config

--- update_system.sh
+++ update_system.sh
@@ -3,6 +3,9 @@ set -xeu
 
 NETBSD_IP=192.168.122.211
 
+./build.sh -U -O ./obj -j$(nproc) -m i386 -a i386 -u kernel=GENERIC
 ./build.sh -U -O ./obj -j$(nproc) -m i386 -a i386 -u kernel=LOCKDOC
 scp etc/rc root@$NETBSD_IP:/etc/rc
+scp etc/etc.i386/boot.cfg root@$NETBSD_IP:/boot.cfg
+scp obj/sys/arch/i386/compile/GENERIC/netbsd root@$NETBSD_IP:/netbsd.orig
 scp obj/sys/arch/i386/compile/LOCKDOC/netbsd root@$NETBSD_IP:/netbsd
